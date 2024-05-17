ad6939ca551c06bd8cfc04a2d4780dcd1be243be
Alvaro Navas <ziritrion@gmai.com>
2024-05-17 13:23:59 +0200

    Finished lesson 19 and section 3 in general

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,14 +4,14 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
 from langchain_openai import AzureChatOpenAI
-#from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
+from typing import Tuple
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
 
@@ -41,10 +41,9 @@ Given the LinkedIn information {information} about a person, I want you to creat
         temperature=0,
     )
 
-    #chain = LLMChain(llm=llm, prompt=summary_prompt_template) # DEPRECATED
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={'information': linkedin_data})
-    print(res)
+    res: Summary = chain.invoke(input={'information': linkedin_data})
+    return res, linkedin_data.get("profile_pic_url")
 
 if __name__ == "__main__":
     print("Ice Breaker")
